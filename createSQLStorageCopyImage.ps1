0a2d0b0afd5b12b5b8c8fce298183648a93ee568
navalev <navaisman@gmail.com>
2016-07-13 13:18:53 +0300

    split resource creation
    1. SQL storage will be created with powershell and image vhd copied to
    it
    2. storage & ip addresses
    3. vnet - existing or new

---
+++ createSQLStorageCopyImage.ps1
@@ -0,0 +1,10 @@
+﻿param($ResourceGroup, $NewStorageAccountName, $NewContainerName, $Location, $SourceStorageAccount, $SourceStorageKey, $SourceContainerName, $SourceBlobName, $DestBlobName)
+
+New-AzureRmResourceGroup -Name $ResourceGroup -Location $Location
+$storage = New-AzureRmStorageAccount -Location $Location -Name $NewStorageAccountName -ResourceGroupName $ResourceGroup -SkuName Premium_LRS
+New-AzureStorageContainer -Name $NewContainerName -Context $storage.Context
+$SourceStorageContext = New-AzureStorageContext  –StorageAccountName $SourceStorageAccount -StorageAccountKey $SourceStorageKey
+$BlobCopy = Start-CopyAzureStorageBlob -Context $SourceStorageContext -SrcContainer $SourceContainerName -SrcBlob $SourceBlobName -DestContext $storage.Context -DestContainer $NewContainerName -DestBlob $DestBlobName
+
+return $BlobCopy.ICloudBlob.SnapshotQualifiedStorageUri.PrimaryUri;
+
